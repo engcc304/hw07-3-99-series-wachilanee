92146fb4f6417b4e195ba734226c92af8d750623
wachilanee <wachilanee47@gmail.com>
2023-08-16 21:49:24 +0700

    hw7-3

--- Homework/HW07/hw07-3-series.cpp
+++ Homework/HW07/hw07-3-series.cpp
@@ -27,17 +27,17 @@ int main(){
 
     
     do{
-        
+        i++;
         n1 = n1 * 10 + 9 ;
         sum += n1;
         
         printf("%d", n1);
-        i++;
+        
         if(i <= n-1 && i != n){
             printf(" + ");
         }
 
-    } while (i <= n);
+    } while (i < n);
     printf("\nSum = %d", sum);
     
     return 0;
